8d0bfc226c43562a6676f12b19d8a3e985547d10
Jewel-Thomas <jewelthomas20@gmail.com>
2023-07-27 11:57:00 +0530

    commit

--- IGDC/Assets/Scripts/ConnectToServer.cs
+++ IGDC/Assets/Scripts/ConnectToServer.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.SceneManagement;
 
-public class ConnectToServer : MonoBehaviour
+public class ConnectToServer : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnConnectedToMaster()
     {
-        
+        PhotonNetwork.JoinLobby();
     }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Connected!");
+        SceneManager.LoadScene(1);
+    }
+
+   
 }
