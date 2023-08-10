8dac16a90d5ee45776ffa00c263530acfd76af72
George J <georgejambooken@gmail.com>
2023-08-10 10:25:03 +0530

    added powerup prefab

---
+++ IGDC/Assets/Scripts/PowerupManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupManager : MonoBehaviour
+{
+    public GameObject[] PowerUps;
+    public Transform[] SpawnLocations;
+    bool powerspawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        powerspawn=false;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(powerspawn=True)
+        {
+            StartCoroutine(PowerCoolDown());
+        }
+        
+    }
+    /*IEnumerator PowerCoolDown()
+    {
+
+    }*/
+}
