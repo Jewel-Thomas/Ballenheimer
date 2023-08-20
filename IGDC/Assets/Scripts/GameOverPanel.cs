d4e88caeb87931881b9a45b02a7e025d4d616511
George J <georgejambooken@gmail.com>
2023-08-20 18:26:06 +0530

    Title and controls

---
+++ IGDC/Assets/Scripts/GameOverPanel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverPanel : MonoBehaviour
+{
+    string SceneName= "DodgeBall AI";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ IGDC/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    string SceneName= "DodgeBall AI";
+    public GameObject MainMenuPan,Instructions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void SeeControls()
+    {
+        MainMenuPan.SetActive(false);
+        Instructions.SetActive(true);
+    }
+    public void closeButton()
+    {
+        Instructions.SetActive(false);
+        MainMenuPan.SetActive(true);
+    }
+}
